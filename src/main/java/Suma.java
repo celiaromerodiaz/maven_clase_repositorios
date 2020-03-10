8260d071bde5eedf00f6db691f575050a91f7b08
Celia Romero <celiaromerodiaz22@gmail.com>
2020-03-10 13:49:54 +0100

    Suma de double añadida

--- src/main/java/Suma.java
+++ src/main/java/Suma.java
@@ -1,5 +1,9 @@
 public class Suma {
-    public static void main(String[] args) {
+    public int sumar(int a, int b){
+        return a + b;
+    }
 
+    public double sumar(double a, double b){
+        return a + b;
     }
 }
